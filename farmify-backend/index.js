e8a5d9a55ba23755326c8a65f6a8cadcf969b72b
Abhilash Yasoji <yasojiabhilash@gmail.com>
2025-11-28 22:28:08 +0530

    farmify backend update

--- farmify-backend/index.js
+++ farmify-backend/index.js
@@ -17,7 +17,11 @@ const app = express();
 const PORT = process.env.PORT || 5000;
 
 // Middleware
-app.use(cors());
+app.use(cors({
+  origin: 'https://farmify-frontend-indol.vercel.app/',
+  credentials: true
+}));
+
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
