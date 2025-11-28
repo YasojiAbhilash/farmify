1208aef105d2129b1e63eb8e96f00b19d543a057
Abhilash Yasoji <yasojiabhilash@gmail.com>
2025-11-28 22:41:42 +0530

    farmify backend update

--- farmify-backend/index.js
+++ farmify-backend/index.js
@@ -34,7 +34,7 @@ app.get("/", (req, res) => {
 });
 app.use("/api/auth", authRoutes);
 app.use("/api/users", userRoutes);
-app.use("/api/products", productRoutes);
+app.use("/products", productRoutes);
 app.use("/api/orders", orderRoutes);
 app.use("/api/messages", messageRoutes);
 app.use("/api/categories", categoryRoutes);
